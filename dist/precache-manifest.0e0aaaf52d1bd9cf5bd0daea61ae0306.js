c27a1ff7c78b5ca8d21307c1acd54d1d2d4ad1f1
devDeerHead <dev@deerhead.net>
2020-06-21 18:00:41 +0200

    build

---
+++ dist/precache-manifest.0e0aaaf52d1bd9cf5bd0daea61ae0306.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a27fe78bf6756825271b",
+    "url": "/css/app.392ae940.css"
+  },
+  {
+    "revision": "9205eede3372befbe783",
+    "url": "/css/chunk-4572d864.c33cafc0.css"
+  },
+  {
+    "revision": "bc970358d67885456515",
+    "url": "/css/chunk-vendors.b2eb3882.css"
+  },
+  {
+    "revision": "b5d6c0141a41343a22535c33f6c4d49b",
+    "url": "/fonts/Heebo-Bold.b5d6c014.ttf"
+  },
+  {
+    "revision": "1dd10c9c73ad7d15b43706c19c24e339",
+    "url": "/fonts/Heebo-Light.1dd10c9c.ttf"
+  },
+  {
+    "revision": "2c138464ef7346e3e2d03797c18bd57f",
+    "url": "/fonts/Heebo-Medium.2c138464.ttf"
+  },
+  {
+    "revision": "e00e2a77dd88a8fe75573a5d993af76a",
+    "url": "/fonts/Ubuntu-Bold.e00e2a77.ttf"
+  },
+  {
+    "revision": "277289c53af7cb469c1dc5dca3adca35",
+    "url": "/fonts/Ubuntu-Light.277289c5.ttf"
+  },
+  {
+    "revision": "8e22c2a6e3a3c679787e763a97fa11f7",
+    "url": "/fonts/Ubuntu-Medium.8e22c2a6.ttf"
+  },
+  {
+    "revision": "254eef03a646df3920eb160d02562cc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a2fd8fada721801ca0b",
+    "url": "/js/about.ce3419c7.js"
+  },
+  {
+    "revision": "a27fe78bf6756825271b",
+    "url": "/js/app.8cbd4e78.js"
+  },
+  {
+    "revision": "9205eede3372befbe783",
+    "url": "/js/chunk-4572d864.00116fe0.js"
+  },
+  {
+    "revision": "bc970358d67885456515",
+    "url": "/js/chunk-vendors.f56f95fb.js"
+  },
+  {
+    "revision": "33ba68ebd9622be4530069df475d5f0a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
